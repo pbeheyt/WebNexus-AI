47e63f58d7363fca4d49d99cecfc31b5ba5ae501
Pierre BEHEYT <pierre.beheyt.bba@edhec.com>
2025-03-16 08:18:06 +0100

    update pdf type content ui

--- src/popup/ui/ContentTypeView.js
+++ src/popup/ui/ContentTypeView.js
@@ -12,16 +12,18 @@ export default class ContentTypeView {
     let typeText = 'Unknown Content';
     let typeClass = 'unknown';
     let iconSvg = '';
+    let borderColor = '';
     
     switch (contentType) {
       case CONTENT_TYPES.YOUTUBE:
         typeText = 'YouTube Video';
         typeClass = 'youtube';
+        borderColor = '#FF0000';
         iconSvg = `
           <svg class="youtube-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M22.54 6.42C22.4212 5.94541 22.1793 5.51057 21.8387 5.15941C21.498 4.80824 21.0708 4.55318 20.6 4.42C18.88 4 12 4 12 4C12 4 5.12 4 3.4 4.46C2.92925 4.59318 2.50198 4.84824 2.16135 5.19941C1.82072 5.55057 1.57879 5.98541 1.46 6.46C1.14521 8.20556 0.991235 9.97631 1 11.75C0.988687 13.537 1.14266 15.3213 1.46 17.08C1.57879 17.5546 1.82072 17.9894 2.16135 18.3406C2.50198 18.6918 2.92925 18.9468 3.4 19.08C5.12 19.54 12 19.54 12 19.54C12 19.54 18.88 19.54 20.6 19.08C21.0708 18.9468 21.498 18.6918 21.8387 18.3406C22.1793 17.9894 22.4212 17.5546 22.54 17.08C22.8524 15.3398 23.0063 13.5747 23 11.8C23.0113 10.0129 22.8573 8.22856 22.54 6.47" 
-              stroke="#FF0000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M9.75 15.02L15.5 11.75L9.75 8.48001V15.02Z" fill="#FF0000" stroke="#FF0000" 
+              stroke="${borderColor}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9.75 15.02L15.5 11.75L9.75 8.48001V15.02Z" fill="${borderColor}" stroke="${borderColor}" 
               stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
         `;
@@ -29,9 +31,10 @@ export default class ContentTypeView {
       case CONTENT_TYPES.REDDIT:
         typeText = 'Reddit Post';
         typeClass = 'reddit';
+        borderColor = '#FF4500';
         iconSvg = `
           <svg class="reddit-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle class="st0" cx="12" cy="12" r="10" fill="#FF4500"/>
+            <circle class="st0" cx="12" cy="12" r="10" fill="${borderColor}"/>
             <path class="st1" fill="#FFFFFF" d="M18.6,12c0-0.8-0.7-1.4-1.5-1.4c-0.4,0-0.7,0.2-0.9,0.4c-1.1-0.8-2.4-1.2-3.8-1.2l0.6-3.1l2.1,0.4
               c0.1,0.5,0.5,0.9,1.1,0.9c0.5,0,0.9-0.5,0.9-1.1c0-0.6-0.5-1-1.1-0.9c-0.3,0-0.6,0.2-0.7,0.5l-2.4-0.5c-0.2,0-0.3,0.1-0.4,0.2L12,9.1
               c-1.4,0-2.7,0.4-3.8,1.2c-0.6-0.5-1.5-0.5-2,0.1c-0.5,0.6-0.5,1.5,0.1,2c0.1,0.1,0.2,0.2,0.4,0.3c0,0.1,0,0.3,0,0.4
@@ -45,19 +48,21 @@ export default class ContentTypeView {
       case CONTENT_TYPES.PDF:
         typeText = 'PDF Document';
         typeClass = 'pdf';
+        borderColor = '#F40F02';
         iconSvg = `
           <svg class="pdf-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M14 2H6C4.89543 2 4 2.89543 4 4V20C4 21.1046 4.89543 22 6 22H18C19.1046 22 20 21.1046 20 20V8L14 2Z" 
-              stroke="#F40F02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M14 2V8H20" stroke="#F40F02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M9 13H15" stroke="#F40F02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M9 17H12" stroke="#F40F02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              stroke="${borderColor}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M14 2V8H20" stroke="${borderColor}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9 13H15" stroke="${borderColor}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9 17H12" stroke="${borderColor}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
         `;
         break;
       case CONTENT_TYPES.GENERAL:
         typeText = 'Web Content';
         typeClass = 'general';
+        borderColor = 'var(--color-primary)';
         iconSvg = `
           <svg class="general-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="3" y="3" width="18" height="18" rx="2" stroke="currentColor" stroke-width="2"/>
@@ -70,6 +75,7 @@ export default class ContentTypeView {
       default:
         typeText = 'Unsupported Content';
         typeClass = 'unsupported';
+        borderColor = 'currentColor';
         iconSvg = `
           <svg class="general-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 8V12M12 16H12.01M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" 
@@ -80,5 +86,10 @@ export default class ContentTypeView {
     
     this.element.className = `content-type ${typeClass}`;
     this.element.innerHTML = iconSvg + typeText;
+    
+    // Apply the border color dynamically
+    this.element.style.borderLeftColor = borderColor;
+    this.element.style.borderLeftWidth = '3px';
+    this.element.style.borderLeftStyle = 'solid';
   }
 }
