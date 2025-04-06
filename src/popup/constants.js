1245671d782e407cc3a181cdc81df777fe1ffd2d
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-04-06 15:44:22 +0200

    remove old popup code

--- src/popup/constants.js
+++
@@ -1,6 +0,0 @@
-// src/popup/constants.js
-// Import all shared constants
-import { CONTENT_TYPES, STORAGE_KEYS, PROMPT_TYPES, SHARED_TYPE } from '../shared/constants.js';
-
-// Export them for backwards compatibility
-export { CONTENT_TYPES, STORAGE_KEYS, PROMPT_TYPES, SHARED_TYPE };
