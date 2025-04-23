aad01e2fafa1d7c604199f391e9a09b7ca375124
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-04-23 07:18:12 +0200

    switch hover and select color paltfrom card dark theme

--- src/components/layout/PlatformCard.jsx
+++ src/components/layout/PlatformCard.jsx
@@ -22,8 +22,8 @@ export function PlatformCard({
       className={`
         flex flex-col items-center justify-center p-2 rounded-md transition-all duration-200 select-none cursor-pointer
         ${selected
-          ? 'bg-gray-200 dark:bg-gray-800'
-          : 'hover:bg-gray-100 dark:hover:bg-gray-700'
+          ? 'bg-gray-200 dark:bg-gray-700'
+          : 'hover:bg-gray-100 dark:hover:bg-gray-800'
         }
       `}
       onClick={handleClick}
