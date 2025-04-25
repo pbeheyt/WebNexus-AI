60d866214cf29a1361b7d599f4ea201489c1daf7
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-04-25 15:26:10 +0200

    update slider platform layout

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -199,12 +199,12 @@ export function Popup() {
       </AppHeader>
 
       {/* Platform Selector */}
-      <div className="mt-2">
+      <div className="mt-5">
          <PlatformSelector disabled={!isSupported || contentLoading || isProcessingContent || isProcessing || !isInjectable} />
       </div>
 
       {/* Unified Input */}
-      <div className="mt-4">
+      <div className="mt-5">
         {/* Container for badge/info OR non-injectable message */}
         <div className="flex justify-between items-center mb-1.5 px-3 min-h-[28px]">
           {!contentLoading && (
