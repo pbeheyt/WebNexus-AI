14b1238dc9f76b514c03a1517f32bbd8c2f4f5fc
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-05-08 21:25:00 +0200

    rm unsued import logger token counter

--- src/sidebar/components/TokenCounter.jsx
+++ src/sidebar/components/TokenCounter.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-import { logger } from '../../shared/logger';
 import { Tooltip } from '../../components/layout/Tooltip';
 import { InputTokenIcon, OutputTokenIcon, ChevronUpIcon } from '../../components';
 
