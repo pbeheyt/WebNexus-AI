76523e86fd7c496c31ae31e967d3a04caa60929c
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-05-10 22:23:26 +0200

    rm selection componentsettings file

--- src/settings/components/ui/common/SelectorSection.jsx
+++
@@ -1,5 +0,0 @@
-// This file's component (SelectorSection) has been refactored out.
-// Its functionality is now achieved by using SettingsCard directly in:
-// - src/settings/components/tabs/DataManagementTab.jsx
-// - src/settings/components/ui/api/ModelParametersSettings.jsx
-// This file can be safely deleted from the project.
