64b61e45b0bbff732fd0d8d059bfa9dc649ab1cc
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-06-02 14:43:57 +0200

    update tooltip props with ref in popup files

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -407,6 +407,8 @@ export function Popup() {
                           }
                           aria-describedby='include-context-tooltip'
                           className='inline-flex items-center'
+                          tabIndex={0}
+                          role="button"
                         >
                           <Toggle
                             id='include-context-toggle'
