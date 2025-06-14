5f66ad89289d6996be706e2629ea945950a0fdee
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-06-14 22:33:46 +0200

    rm useless context from debounce function

--- src/shared/utils/debounce-utils.js
+++ src/shared/utils/debounce-utils.js
@@ -4,10 +4,9 @@ export function debounce(func, wait) {
   let timeout;
 
   const debouncedFunction = function executedFunction(...args) {
-    const context = this; // Capture context
     const later = () => {
       timeout = null; // Clear timeout ID *before* calling func
-      func.apply(context, args); // Call original function
+      func(...args); // Call original function using spread syntax
     };
     clearTimeout(timeout); // Clear the previous timeout
     timeout = setTimeout(later, wait); // Set a new timeout
